2b21f02637a134fca8a61754c4b152a5d28f1e11
Nathan <natefintak@gmail.com>
2025-03-01 18:17:34 -0500

    in progress

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalc.java
@@ -1,6 +1,38 @@
 package com.zipcodewilmington.scientificcalculator;
 //first input = x, second input = y
+import java.util.Scanner;
+
 public class BasicCalc {
+    Scanner scanner = new Scanner(System.in);
+    double x;
+    double y;
+    double result;
+
+
+    public String Basic(String[] args) {
+
+        System.out.println("Basic Calculator:");
+        System.out.println("First number: ");
+        double input1 = scanner.nextDouble();
+        System.out.println("Operator: ");
+        String prompt = scanner.nextLine();
+        char func = prompt.charAt(0);
+        System.out.println("Second number: ");
+        double input2 = scanner.nextDouble();
+        x = input1;
+        y = input2;
+
+        switch (func) {
+            case '+':
+                result = addTwoNumbers(x, y);
+                System.out.println(x + " + " + y + " = " + result);
+                break;
+            case '-':
+                result = subTwoNumbers(x, y);
+                System.out.println(x + " - " + y + " = " + result);
+                break;
+        }
+    }
     double addTwoNumbers(double x, double y) {
         return (x + y);
     }
@@ -25,6 +57,4 @@ public class BasicCalc {
     double variableExp(double x, double y) {
         return (Math.pow(x, y));
     }
-
-
 }

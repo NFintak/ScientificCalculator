f77906fb3b8bc558fcaf6300b8cd5d14b09efe40
jwheller0013 <jwheller0013@yahoo.com>
2025-03-01 13:12:31 -0500

    Input functions for Trig, working on output

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
@@ -1,5 +1,34 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class SciCalc {
+    System.out.println(Cosine);
 }
 //x equals first input, y equals second input
+double x=10.0
+//Trig Functions
+public double Sine(x) {
+    return Math.sin(x);
+}
+public double InverseSine(x) {
+    return Math.asin(x);
+}
+public double Cosine(x) {
+    return Math.cos(x);
+}
+public double InverseCosine(x) {
+    return Math.acos(x);
+}
+public double Tangent(x) {
+    return Math.tan(x);
+}
+public double InverseTangent(x) {
+    return Math.atan(x);
+}
+
+//Log Functions
+public double Log(x) {
+    return Math.log(x);
+}
+public double Ln (x) {
+    return (Math.log(x)/0.4342944819);
+}

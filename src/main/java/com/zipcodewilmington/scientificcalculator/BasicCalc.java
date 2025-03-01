fee86b46e12cdac59bc61b5a2001d30b58da0050
Nathan <natefintak@gmail.com>
2025-03-01 11:41:35 -0500

    in progress

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalc.java
@@ -1,4 +1,20 @@
 package com.zipcodewilmington.scientificcalculator;
 //first input = 1, second input = 2
 public class BasicCalc {
+    public static void main(String[] args) {
+
+    }
+
+    int addTwoNumbers(int x, int y) {
+        return (x + y);
+    }
+    int subTwoNumbers(int x, int y) {
+        return (x - y);
+    }
+    int multiplyTwoNumbers(int x, int y) {
+        return (x * y);
+    }
+    int divideTwoNumbers(int x, int y) {
+        return (x / y);
+    }
 }

2d96323f57cc82d7e3c101a98de01da709418189
jwheller0013 <jwheller0013@yahoo.com>
2025-03-04 11:29:17 -0500

    Cleaning up code

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
@@ -2,75 +2,44 @@
 //
 //import java.util.Scanner;
 //
-//public class SciCalc {
-//
-////        public void main(String[] args) {
-////            SciCalc sciCalc = new SciCalc();
-////            Scanner scanner = new Scanner(System.in);
-////            System.out.print("Select: displayMode, memory, trig, degreeRadian, logFunctions, factorial");
-////            String input = scanner.nextLine();
-////            sciCalc.(input);
-////        }
-//
+public class SciCalc {
+double num1;
+
 //    // Change this all to methods only!!
 //    // Selection of modes and types all in main make new objs of SciCalc
-//
-//
-////    public double logFunctions(){
-////        Scanner scanner = new Scanner(System.in);
-////        System.out.print("Select: Log, InverseLog, Ln, and E^x");
-////        String input = scanner.nextLine();
-//
-//        public double log {
-//            return Math.log(x);
-//        }
-//        double inverseLog {
-//            return Math.pow(10, x);
-//        }
-//        double ln {
-//            return (Math.log(x) / 0.4342944819);
-//        }
-//        double ex {
-//            return Math.exp(x);
-//        }
+
+    public double log( double num1) {
+            return Math.log( double num1);
+        }
+        double inverseLog( double num1) {
+            return Math.pow(10, num1);
+        }
+        double ln(num1) {
+            return (Math.log(double num1) / 0.4342944819);
+        }
+        double ex(double num1) {
+            return Math.exp(num1);
+        }
 //
 //
 //    //Switch Display mode it should be a format change
-//
-//    public double displayMode(){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Select: 1. binary, 2. octal, 3. decimal, 4. hexadecimal or 5. Exit");
-//        String input = scanner.nextLine();
-//
-//        switch (input) {
-//            case (1):
-//                Integer.toBinaryString(x);
-//                //something to change it, typing Integer. brought up a list scrolled and found answer...proper?
-//                break;
-//
-//            case (2):
-//                Integer.toOctalString(x);
-//                //sames as with "binary" seems to have built-in function
-//                break;
-//
-//            case (3):
-//                return String.format("%.8f", Double.valueOf(x));
-//            //format x as a string with 8 in this case deciaml points
-//            break;
-//
-//            case (4):
-//                Integer.toHexString(x);
-//                //sames as with "binary" seems to have built-in function
-//                break;
-//
-//            case (5):
-//                return x;
-//            break;
-//        }
-//        scanner.close();
-//    }
-//
-//
+
+    public  int BinaryString (num1) {
+           return Integer.toBinaryString(num1);
+    }
+    public int OctalString (num1) {
+            return Integer.toOctalString(x);
+    }
+
+    public double Decimal (num1) {
+        return String.format("%.8f", Double.valueOf(num1));
+    }
+
+    public double HexString (num1) {
+            return Integer.toHexString(x);
+    }
+
+
 //    //Memory Save
 //    public double memory()
 //    {
@@ -94,93 +63,71 @@
 //    }
 //
 //    //Trig Functions it needs to be called then have user select function then run
-//
-//        public double sine(double num1) {
-//            double result = Math.sin();
-//        }
-//
-//        public double aSine (double num1) {
-//            double result = Math.asin();
-//        }
-//
-//        public double cosine(double num1) {
-//            double result = Math.cos();
-//        }
-//        public double inverseCosine {
-//            double result = Math.acos();
-//        }
-//        public double Tangent {
-//            double result = Math.tan();
-//        }
-//        public double InverseTangent {
-//            double result = Math.atan(x);
-//            return result;
-//        }
-//
-//
+
+        public double sine(double num1) {
+            double result = Math.sin();
+        }
+
+        public double aSine (double num1) {
+            double result = Math.asin();
+        }
+
+        public double cosine(double num1) {
+            double result = Math.cos();
+        }
+        public double inverseCosine {
+            double result = Math.acos();
+        }
+        public double Tangent {
+            double result = Math.tan();
+        }
+        public double InverseTangent {
+            double result = Math.atan(x);
+            return result;
+        }
+
 //        //Switch Trig Units it needs to be called then have user select function then run
-//
-//    public double degreeRadian() {
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.print("Select to 1. Radian, 2. Degree, or 3. Exit");
-//            String input = scanner.nextInt();
-//
-//            switch (input) {
-//                case (1):
-//                    return Math.toRadians(x);
-//                break;
-//
-//                case (2):
-//                    return Math.toDegrees(x);
-//                break;
-//
-//                case (3):
-//                    return x;
-//                break;
-//            }
-//            scanner.close();
-//        }
-//
+
+    public Radians(num1) {
+            return Math.toRadians(num1);
+    }
+
+    public Degrees(num1) {
+        return Math.toDegrees(num1);
+    }
+
+
 //        //Log Functions it needs to be called then have user select function then run
-//
-//    public double logFunctions(){
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.print("Select: Log, InverseLog, Ln, and E^x");
-//            String input = scanner.nextLine();
-//
-//            public double Log {
-//                return Math.log(x);
-//            }
-//            double InverseLog {
-//                return Math.pow(10, x);
-//            }
-//            double Ln {
-//                return (Math.log(x) / 0.4342944819);
-//            }
-//            double ex {
-//                return Math.exp(x);
-//            }
-//            scanner.close();
-//        }
-//
+
+            public double Log(num1) {
+                return Math.log(num1);
+            }
+            public double InverseLog(num1) {
+                return Math.pow(10, num1);
+            }
+            public double Ln(num1) {
+                return (Math.log(num1) / 0.4342944819);
+            }
+            public double Ex(num1) {
+                return Math.exp(num1);
+            }
+
 //    // Factorial Function
-//    public int factorial(){
-//        if (x == 0) {
-//            return 1;
-//        }
-//        int recurse = Factorial(x - 1);
-//        int result = x * recurse;
-//        return result;
-//        }
-//
-//    }
-//
-//    }
-//// Turned off line 8 undo comments mode if wrong
+
+    public Integer factorial(Integer num1){
+          int f = 1;
+          for (int i = 1; i<= num1; i++) {
+              f = f * i
+          }
+          return f ;
+    }
+
+
+
 //    public void selectFunction (){
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Select: displayMode, memory, trig, degreeRadian, logFunctions, factorial");
 //        String input = scanner.nextLine();
 //        sciCalc(input);
-//    }
+    }
 //}

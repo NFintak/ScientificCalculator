a72b5ea762b97c2bdc9ac909f66c5e74238c3238
jwheller0013 <jwheller0013@yahoo.com>
2025-03-05 15:12:23 -0500

    cleaning up code

--- src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SciCalc.java
@@ -43,28 +43,6 @@ String num1;
 //            return Integer.toHexString(x);
 //    }
 
-
-//    //Memory Save
-//    public double memory()
-//    {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Select: 1. M+, 2. MC, 3. MRC, or 4. Exit");
-//        String input = scanner.nextInt();
-//        int z = 0;
-//        //z is the default for Memory
-//        switch (input) {
-//            case 1:
-//                z = x;
-//                return z;
-//            case 2:
-//                z = 0;
-//                return z;
-//            case 3:
-//                x = z;
-//                return x;
-//        }
-//        scanner.close();
-//    }
 //
 //    //Trig Functions it needs to be called then have user select function then run
 
@@ -103,26 +81,6 @@ String num1;
         return Math.toDegrees(num1);
     }
 
-
-////        //Log Functions it needs to be called then have user select function then run
-//
-//            public double Log(String num1) {
-//                double x = Double.parseDouble(num1);
-//                return Math.log(x);
-//            }
-//            public double InverseLog(String num1) {
-//                double x = Double.parseDouble(num1);
-//                return Math.pow(10, x);
-//            }
-//            public double Ln(String num1) {
-//                double x = Double.parseDouble(num1);
-//                return (Math.log(x) / 0.4342944819);
-//            }
-//            public double Ex(String num1) {
-//                double x = Double.parseDouble(num1);
-//                return Math.exp(x);
-//            }
-//
 //    // Factorial Function
 
     public double factorial(double num1){
@@ -134,12 +92,4 @@ String num1;
           return f ;
     }
 
-
-
-//    public void selectFunction (){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Select: displayMode, memory, trig, degreeRadian, logFunctions, factorial");
-//        String input = scanner.nextLine();
-//        sciCalc(input);
     }
-//}
